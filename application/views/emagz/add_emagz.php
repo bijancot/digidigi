88bb7533fc78ffe2ecd460dd9dfaa62940f7d932
devisap <ilham.sagitaputra@gmail.com>
2021-03-04 12:39:49 +0700

    add input radio

--- application/views/emagz/add_emagz.php
+++ application/views/emagz/add_emagz.php
@@ -46,6 +46,17 @@
           <div class="col-9">
             <input type="text" id="linkPubHtml5" class="form-control" name="linkPubHtml5"> 
           </div>
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-secondary active">
+              <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+            </label>
+            <label class="btn btn-secondary">
+              <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+            </label>
+            <label class="btn btn-secondary">
+              <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+            </label>
+          </div>
         </div>
         <div class="form-group row"> 
           <label for="linkquiz" class="col-3 col-form-label">Link Quiz</label>
