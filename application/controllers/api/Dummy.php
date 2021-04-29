afbd610b57d0cfef1c0cefd3214f058280d2d7f8
Ilham Sagita Putra <50571957+devisap@users.noreply.github.com>
2021-04-29 12:18:32 +0700

    update version

--- application/controllers/api/Dummy.php
+++ application/controllers/api/Dummy.php
@@ -91,7 +91,7 @@ class Dummy extends REST_Controller {
     }
   }
   function version_get(){
-    $this->response(['status' => TRUE, 'data' => ['version' => "1.2.3"]], REST_Controller::HTTP_OK);
+    $this->response(['status' => TRUE, 'data' => ['version' => "22"]], REST_Controller::HTTP_OK);
   }
 }
 ?>
