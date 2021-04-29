67cc0c5f71b24c49390dd910c66de515f3beb0de
ut-dev infra <panji@diwangkara.dev>
2021-04-29 15:46:29 +0700

    force https for slider image

--- application/controllers/api/Slider.php
+++ application/controllers/api/Slider.php
@@ -23,7 +23,14 @@ class Slider extends REST_Controller {
           $q->NEWS_IMAGE = $images;
         } else {
           if (isset($q->NEWS_IMAGE)){
+             if(strpos($q->NEWS_IMAGE, 'http') !== false){
+                        $str_img = $q->NEWS_IMAGE;
+                        $new = explode(':',$str_img);
+                        $rr = "https:".$new[1];
+                        $q->NEWS_IMAGE = [$rr];
+                }else{
             $q->NEWS_IMAGE = [$q->NEWS_IMAGE];
+                }
           }
         }
       }
